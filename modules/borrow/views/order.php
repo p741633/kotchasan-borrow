c177c1ea2330fa3324dbaee920195d12d91621c2
PiriyaV <p741633@hotmail.com>
2024-10-02 14:06:09 +0700

    Show borrower detail
    Disable change-status button
    Disable  delivery button

--- modules/borrow/views/order.php
+++ modules/borrow/views/order.php
@@ -53,7 +53,8 @@ class View extends \Gcms\View
             'placeholder' => Language::replace('Fill some of the :name to find', [':name' => '{LNG_Name}, {LNG_Email}, {LNG_Phone}']),
             'title' => '{LNG_Borrower}',
             'value' => $index->borrower,
-            'autofocus' => true
+            'autofocus' => true,
+            'readonly' => true
         ]);
         // borrower_id
         $fieldset->add('hidden', [
@@ -72,24 +73,27 @@ class View extends \Gcms\View
             'itemClass' => 'width50',
             'label' => '{LNG_Transaction No.}',
             'placeholder' => '{LNG_Leave empty for generate auto}',
-            'value' => $index->borrow_no
+            'value' => $index->borrow_no,
+            'readonly' => true
         ]);
         // transaction_date
         $groups->add('date', [
             'id' => 'transaction_date',
             'labelClass' => 'g-input icon-calendar',
-            'itemClass' => 'width50',
+            'itemClass' => 'width50 date-readonly',
             'label' => '{LNG_Transaction date}',
-            'value' => $index->transaction_date
+            'value' => $index->transaction_date,
+            'readonly' => true
         ]);
         $groups = $fieldset->add('groups');
         // borrow_date
         $groups->add('date', [
             'id' => 'borrow_date',
             'labelClass' => 'g-input icon-calendar',
-            'itemClass' => 'width50',
+            'itemClass' => 'width50 date-readonly',
             'label' => '{LNG_Borrowed date}',
-            'value' => $index->borrow_date
+            'value' => $index->borrow_date,
+            'readonly' => true
         ]);
         // return_date
         $groups->add('date', [
@@ -99,6 +103,74 @@ class View extends \Gcms\View
             'label' => '{LNG_Date of return}',
             'value' => $index->return_date
         ]);
+        $groups = $fieldset->add('groups');
+        // borrower_emp_id
+        $groups->add('number', [
+            'id' => 'borrower_emp_id',
+            'labelClass' => 'g-input icon-profile',
+            'itemClass' => 'width50 number-readonly',
+            'label' => '{LNG_Borrower employee id}',
+            'value' => $index->borrower_emp_id,
+            'maxlength' => 6,
+            'readonly' => true,
+            'required' => true
+        ]);
+        // borrower_dept_id
+        $groups->add('text', [
+            'id' => 'borrower_dept_id',
+            'labelClass' => 'g-input icon-profile',
+            'itemClass' => 'width50',
+            'label' => '{LNG_Borrower department id}',
+            'value' => $index->borrower_dept_id,
+            'maxlength' => 4,
+            'readonly' => true,
+            'required' => true
+        ]);
+        $groups = $fieldset->add('groups');
+        // borrower_fname
+        $groups->add('text', [
+            'id' => 'borrower_fname',
+            'labelClass' => 'g-input icon-personnel',
+            'itemClass' => 'width50',
+            'label' => '{LNG_Borrower first name}',
+            'value' => $index->borrower_fname,
+            'maxlength' => 50,
+            'readonly' => true,
+            'required' => true
+        ]);
+        // borrower_lname
+        $groups->add('text', [
+            'id' => 'borrower_lname',
+            'labelClass' => 'g-input',
+            'itemClass' => 'width50',
+            'label' => '{LNG_Borrower last name}',
+            'value' => $index->borrower_lname,
+            'maxlength' => 50,
+            'readonly' => true,
+            'required' => true
+        ]);
+        $groups = $fieldset->add('groups');
+        // borrower_phone
+        $groups->add('number', [
+            'id' => 'borrower_phone',
+            'labelClass' => 'g-input icon-phone',
+            'itemClass' => 'width20 number-readonly',
+            'label' => '{LNG_Borrower phone}',
+            'value' => $index->borrower_phone,
+            'maxlength' => 15,
+            'readonly' => true,
+            'required' => true
+        ]);
+        // borrower_remark
+        $groups->add('text', [
+            'id' => 'borrower_remark',
+            'labelClass' => 'g-input icon-comments',
+            'itemClass' => 'width80',
+            'label' => '{LNG_Borrower remark}',
+            'value' => $index->borrower_remark,
+            'maxlength' => 255,
+        ]);
+        $groups = $fieldset->add('groups');
         $borrow_status = Language::get('BORROW_STATUS');
         $table = '<table class="fullwidth data border"><thead><tr>';
         $table .= '<th>{LNG_Detail}</th>';
@@ -113,9 +185,13 @@ class View extends \Gcms\View
             $table .= '<td class="center">'.$item['num_requests'].'</td>';
             $table .= '<td class="center" id="amount_'.$item['id'].'">'.$item['amount'].'</td>';
             $table .= '<td class="center"><span class="term'.$item['status'].'" id="status_'.$item['id'].'">'.$borrow_status[$item['status']].'</span></td>';
-            $table .= '<td class="center"><a id=delivery_'.$item['borrow_id'].'_'.$item['id'].' class="button icon-outbox green">{LNG_Delivery}</a></td>';
-            $table .= '<td class="center"><a id=return_'.$item['borrow_id'].'_'.$item['id'].' class="button icon-inbox blue">{LNG_Return}</a></td>';
-            $table .= '<td class="center"><a id=status_'.$item['borrow_id'].'_'.$item['id'].' class="button icon-star0 red">{LNG_Status update}</a></td>';
+            // $table .= '<td class="center"><a id=delivery_'.$item['borrow_id'].'_'.$item['id'].' class="button icon-outbox green">{LNG_Delivery}</a></td>';
+            if ($item['status'] !== 3) {
+                $table .= '<td class="center"><a id=return_'.$item['borrow_id'].'_'.$item['id'].' class="button icon-inbox blue">{LNG_Return}</a></td>';
+            } else {
+                $table .= '<td class="center">&nbsp;</td>';
+            }
+            // $table .= '<td class="center"><a id=status_'.$item['borrow_id'].'_'.$item['id'].' class="button icon-star0 red">{LNG_Status update}</a></td>';
             $table .= '</tr>';
         }
         $table .= '</tbody>';
